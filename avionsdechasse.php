c7ca7e27e668c7b48c60d006e0908de5c0fc295c
Romein A <romein@localhost.localdomain>
2016-06-30 21:57:12 +0200

    initial

---
+++ avionsdechasse.php
@@ -0,0 +1,219 @@
+#!/bin/usr/php
+<?php
+define('AC_URL', 'http://www.mensquare.com/avionsdechasse/avions/$page?type=last');
+define('AC_PATH', '/home/romein/Pictures/Avions de Chasse/');
+define('AC_DB', '.avions.db');
+define('AC_PREVENT_DUPLICATE', true); // set to false to download every pictures, even if it is recurrent.
+
+$increment=0;
+function png2jpg($originalFile, $outputFile, $quality=95) {
+	if($image = imagecreatefrompng($originalFile))
+	{
+		imagejpeg($image, $outputFile, $quality);
+		imagedestroy($image);
+		$increment=0;
+	}
+	else
+		if(!(boolean)$increment++)
+			png2jpg($originalFile, $outputFile, $quality);
+	return true;
+}
+
+function forcejpg($originalFile, $outputFile, $quality=97)
+{
+	global $increment;
+	
+	set_error_handler(function ($no, $msg, $file, $line) {
+		throw new ErrorException($msg, 0, $no, $file, $line);
+	});
+	try{
+		$image = imagecreatefromstring(file_get_contents($originalFile));
+		$increment=0;
+	}catch(Exception $e)
+	{
+		if(!(boolean)$increment++) // try three times
+			forcejpg($originalFile, $outputFile, $quality);
+	}
+	if(imagejpeg($image, $outputFile, $quality))
+		if(imagedestroy($image))
+			return true;
+
+			
+	return false;
+}
+
+// function getthelastfile()
+// {
+// 	$files = glob('*.jpg');
+// 	array_walk($files, function(&$item){
+// 		$item = substr($item, 0, strpos($item, '.'));
+// 	});
+// 	$files = array_flip($files);
+// 	//print count($files);
+// 	krsort($files, SORT_NUMERIC);
+// 	reset($files);
+// 	$files = array_fill(0, 1, array_keys($files));
+// 	return $files[0][0];
+// }
+
+function getDirectory()
+{
+	$files = glob('*.jpg');
+	array_walk($files, function(&$item){
+		$item = substr($item, 0, strpos($item, '.'));
+	});
+	return $files;
+// 	$files = array_flip($files);
+// 	//print count($files);
+// 	krsort($files, SORT_NUMERIC);
+// 	reset($files);
+// 	$files = array_fill(0, 1, array_keys($files));
+// 	return $files;
+}
+
+function putToDb($f)
+{
+	return file_put_contents(AC_DB, $f."\n", FILE_APPEND);
+}
+
+function inDb($f)
+{
+// 	static $db = null;
+// 	if(is_null($db))
+// 	$db = file(AC_DB);
+	return in_array($f, file(AC_DB));
+}
+
+if(!is_dir(AC_PATH))
+	mkdir(AC_PATH);
+chdir(AC_PATH);
+
+// $i = isset($argv[1]) ? 1 : getthelastfile();
+
+// $end = false;
+$alreadyDownloaded = getDirectory();
+
+if(@$argv[1] == 'DB')
+{ // Rebuild the MD5 database, which prevent duplicate pictures
+	@unlink(AC_DB);
+	$db = array();
+	sort($alreadyDownloaded, SORT_NUMERIC);
+	foreach($alreadyDownloaded as $f)
+	{
+		$f .= '.jpg';
+		$md5 = md5_file($f);
+		if(in_array($md5, $db))
+			print 'rm '.$f."\n";
+		else
+			$db[] = $md5;
+	}
+	file_put_contents(AC_DB, implode("\n", $db));
+	exit();
+}
+
+$page = 1;
+$tictac = 0;
+while($tictac != (isset($argv[1]) ? $argv[1] : 3))
+{
+	$url = str_replace('$page', $page > 1 ? 'page/'.$page : '', AC_URL);
+// 	print "DEBUG : URL = $url\n";
+	$raw = file_get_contents($url);
+// 	print "DEBUG : RAW successful, here : $raw\n";
+	//http://i0.wp.com/public.avionsdechasse.org/images/sources/2016/01/20160128081501_1_carre.jpg
+	if(preg_match_all('/(http:\/\/[a-z0-9]+\.wp\.com\/public\.avionsdechasse\.org\/images\/sources\/[0-9]{4}\/[0-9]{2}\/[0-9]{14}[_0-9]*)_carre\.jpg.+\#([0-9]+)</Us', $raw, $matches))
+	{
+// 		print "DEBUG : MATCHES = ".print_r($matches, true);
+		$diff=array_diff($matches[2], $alreadyDownloaded);
+// 		print_r($diff);
+		if(!empty($diff))
+		{
+		
+			$tictac = 0;
+			foreach(array_intersect($matches[2], $diff) as $key => $number)
+			{
+				$dl = $matches[1][$key];
+				$file = $number.'.jpg';
+// 				print "DEBUG : dl = $dl\n";
+// 				print "DEBUG : file = $file\n";
+				if(AC_PREVENT_DUPLICATE)
+					if(inDb($file))	
+						continue; // Do not download if duplicate.
+				if(!forcejpg($dl, $file))
+				{
+					@unlink($file);
+					print "An error occured when downloading avion #$number.\n";
+				}
+				else
+					if(AC_PREVENT_DUPLICATE)
+						putToDb($file);
+			}
+		}
+		else
+			$tictac++;
+		$page++;
+	}
+	else
+	{
+		print "No avion found...";
+		break;
+	}
+}
+
+exit();
+// Obsolete
+
+// while(/*true*/ false)
+// {
+// 	$file = $i.'.jpg';
+// 	$url = AC_URL.$i++;
+// 
+// 	if(file_exists(realpath($file)))
+// 	{
+// 		$end = 0;
+// 		continue; // skip on already downloaded pictures
+// 	}
+// // 	print $url;
+// 	$html = file_get_contents($url);
+// 	if(!preg_match('/<title>\#[0-9]+ .+<\/title>/', $html))
+// 	{
+// 		if($end != 4)
+// 		{
+// 			print $end++ .'... ';
+// 			continue;
+// 		}
+// 		else
+// 		{
+// 			print 'Ending on #'.$i-1 ." ($url)\n";
+// 			break; // quit if we're redirecting to home page
+// 		}
+// 	}
+// 	$end = 0;
+// 	
+// 	// http://avionsdechasse.org/images/images_sources/20130831125613_0_53.jpg
+// 	print 'Downloading #'.$file.'... ';
+// 	if(!preg_match('/http:\/\/(public.)?avionsdechasse.org\/images\/[a-z0-9_\/]+\.(jpg|png)/', $html, $matches))
+// 		print 'Warning : No picture found at '.$url."\n";
+// 
+// /*	if(strrchr($matches[0], '.') == '.png')
+// 		png2jpg($matches[0], $file);
+// 	else
+// 		if(!copy($matches[0], $file))
+// 		{
+// 			@unlink($file); //incomplete file
+// 			print 'error occured.'."\n";
+// 		}else
+// 			print 'done.'."\n";*/
+// 			
+// 	if(forcejpg($matches[0], $file))
+// 		print 'done.'."\n";
+// 	else
+// 	{
+// 		@unlink($file);
+// 		print "error occured.\n";
+// 	}
+// 	unset($html);
+// 	continue;
+// }
+
+
+?>
---
+++ bonjourmadame.php
@@ -0,0 +1,211 @@
+#!/usr/bin/php
+<?php
+/* Usage :
+ *  php bonjourmadame.php 				Will download the latest bonjourmadame via the RSS feed
+ *  php bonjourmadame.php [pageno]		Will download the madames from the archives page, and loop it by [pageno] times (~50 madames per page)
+ * Requires : simple_html_dom.php
+*/
+
+define('DISPLAY_TODAY_ONLY', true); // set to true to display only the BM of the day. false => cycle des BM aléatoire.
+
+define('BM_URL', 'http://bonjourmadame.fr');
+// define('RSS_URL', BM_URL.'/rss');
+define('RSS_URL', 'http://feeds2.feedburner.com/BonjourMadame');
+define('ARCHIVE_URL', BM_URL.'/archive');
+define('IMAGE_URL', BM_URL.'/image');
+define('DOWNLOAD_DIR', '/home/romein/Pictures/BonjourMadame');
+define('PICTURE_SIZE', 1280);
+require('simple_html_dom.php');
+
+
+class BlogPost
+{
+    var $date;
+    var $ts;
+    var $link;
+
+    var $title;
+    var $text;
+}
+
+class BlogFeed
+{
+    var $posts = array();
+
+    function __construct($file_or_url)
+    {
+        $file_or_url = $this->resolveFile($file_or_url);
+        if (!($x = simplexml_load_file($file_or_url)))
+            return;
+
+        foreach ($x->channel->item as $item)
+        {
+            $post = new BlogPost();
+            $post->date  = (string) $item->pubDate;
+            $post->ts    = @strtotime($item->pubDate);
+            $post->link  = (string) $item->link;
+            $post->title = (string) $item->title;
+            $post->text  = (string) $item->description;
+
+            // Create summary as a shortened body and remove images, 
+            // extraneous line breaks, etc.
+            $post->summary = $this->summarizeText($post->text);
+
+            $this->posts[] = $post;
+        }
+    }
+
+    private function resolveFile($file_or_url) {
+        if (!preg_match('|^https?:|', $file_or_url))
+            $feed_uri = $_SERVER['DOCUMENT_ROOT'] .'/shared/xml/'. $file_or_url;
+        else
+            $feed_uri = $file_or_url;
+
+        return $feed_uri;
+    }
+
+    private function summarizeText($summary) {
+        $summary = strip_tags($summary);
+
+        // Truncate summary line to 100 characters
+        $max_len = 100;
+        if (strlen($summary) > $max_len)
+            $summary = substr($summary, 0, $max_len) . '...';
+
+        return $summary;
+    }
+}
+$increment=0;
+function png2jpg($originalFile, $outputFile, $quality) {
+global $increment;
+	if($image = imagecreatefrompng($originalFile))
+	{
+		imagejpeg($image, $outputFile, $quality);
+		imagedestroy($image);
+		$increment=0;
+	}
+	else
+		if(!(boolean)$increment++)
+			png2jpg($originalFile, $outputFile, $quality);
+}
+
+function parse_all($string){
+	preg_match_all('/http:\/\/w?w?w?\.?bonjourmadame\.fr\/post\/([0-9]+)/', $string, $matches);
+	$_id = 0;
+	foreach($matches[1] as $id)
+		download_id('/'.$_id = $id);
+	//return $_id;
+	preg_match('/\/archive\?before_time=([0-9]+)/', $string, $matches);
+	return $matches[0];
+}
+
+
+function download_id($id, $imgurl=null){
+	if(!substr($id, 0, 1)=='/')
+		$id = '/'.$id;
+
+	$file = DOWNLOAD_DIR.$id.'.jpg';
+	print( 'Downloading image id '.$id." at url <".$imgurl."> ..." );
+	if(file_exists($file))
+	{
+		print 'already exists !'."\n";
+		return false;
+	}
+	if(is_null($imgurl))
+	{
+		$imgurl = IMAGE_URL.$id;
+		$imghtml = file_get_html($imgurl);
+		$finalurl = $imghtml->find('img[id=content-image]', 0)->{"data-src"};
+	}
+	else
+		$finalurl = $imgurl;
+	if(strrchr($finalurl, '.') == '.png')
+	{
+		if(!file_exists($file))
+			png2jpg($finalurl, $file, 95);
+	}else
+		if(!file_exists($file))
+			if(!copy($finalurl, $file))
+			{
+				@unlink($file); //incomplete file
+				print 'error occured.';
+			}else
+				print 'done.';
+	print "\n";
+	if(isset($imghtml)) unset ($imghtml);
+	return true;
+}
+
+
+if(!empty($argv[1]))
+{
+	$next_page = parse_all(file_get_contents(ARCHIVE_URL));
+	if($argv[1] > 1)
+	for($i = 1; $i<= $argv[1]; $i++)
+	{
+		print "\nPage $i\n";
+		$next_page = parse_all(file_get_contents(BM_URL.$next_page)); // format : /archive?before_time=0-9
+	}
+}
+
+
+$rss = new BlogFeed(RSS_URL);
+foreach($rss->posts as $p)
+{
+	//print $p->link."\n";
+	$id = strrchr($p->link, '/');
+	//http://41.media.tumblr.com/de2babab556c386245f5c5707143a9f8/tumblr_nv69p6fH611qzy9ouo1_500.jpg
+	preg_match('#(https?:\/\/[0-9]{1,3}.media.tumblr.com\/[a-f0-9]{32}\/tumblr_[a-zA-Z0-9_]+)_500.(jpe?g|png)#', $p->text, $match);
+	$url = $match[1].'_'.PICTURE_SIZE.'.'.$match[2];
+	download_id($id, $url);
+}
+
+exit();
+
+
+// Generator for xml backgrounds.
+chdir(DOWNLOAD_DIR);
+//$dir = dirname(__FILE__);
+$dir = 'BonjourMadame';
+
+define('DEFAULT_DURATION', 600); //10m
+
+$files = glob('./*.jpg');
+usort($files, function($a, $b) {
+    return strcmp($b, $a);
+});
+
+
+exit();
+
+
+if(DISPLAY_TODAY_ONLY)
+	$files = array($files[0]);   //// ONLY THE MADAME OF TODAY !
+
+
+//array_multisort(array_map('filemtime', $files), SORT_NUMERIC, SORT_ASC, $files);
+/*$out = '<starttime>
+    <year>2013</year>
+    <month>09</month>
+    <day>11</day>
+    <hour>00</hour>
+    <minute>00</minute>
+    <second>00</second>
+  </starttime>
+<!-- This animation will start at midnight. -->';*/
+$out = '';
+for($i = 0; $i < count($files); $i++)
+	$out .= '
+	<static>
+    <duration>'.DEFAULT_DURATION.'</duration>
+    <file>'.realpath($files[$i]).'</file>
+  </static>
+    <transition>
+    <duration>0.5</duration>
+    <from>'.realpath($files[$i]).'</from>
+    <to>'.realpath(isset($files[$i+1]) ? $files[$i+1] : $files[0]).'</to>
+  </transition>';
+  
+  
+file_put_contents('../'.$dir.'.xml', '<background>'.$out.'</background>');
+
